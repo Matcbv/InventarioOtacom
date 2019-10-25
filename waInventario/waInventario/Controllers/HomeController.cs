89917decedcced9dc6fc533e12e1ac9e81432639
Matcbv <53276628+Matcbv@users.noreply.github.com>
2019-10-25 15:49:37 -0300

    COMMIT 25/10/2019 - 1 - Criação do projeto

---
+++ waInventario/waInventario/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace waInventario.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
